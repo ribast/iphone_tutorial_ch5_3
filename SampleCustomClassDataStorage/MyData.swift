4c2928ee4d53b868587b6ccffbe93b68b2f94c37
田島諒 <ryo_tajima@43x235x140x85.ap43.ftth.ucom.ne.jp>
2018-12-10 23:49:40 +0900

    MyDataクラス作成

---
+++ SampleCustomClassDataStorage/MyData.swift
@@ -0,0 +1,13 @@
+//
+//  MyData.swift
+//  SampleCustomClassDataStorage
+//
+//  Created by 田島諒 on 2018/12/10.
+//  Copyright © 2018 Ribast. All rights reserved.
+//
+
+import Foundation
+
+class MyData {
+    var valueString: String?
+}
